f356dc6b7226141d68a70386b69346c4a869f947
DESKTOP-3I4UA3R\AREGBESOLA .BOLU <justicearegbesola@gmail.com>
2026-07-12 19:58:51 +0100

    sign up update

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,14 +3,24 @@ import { getFirestore, type Firestore } from "firebase/firestore";
 import { getAuth, type Auth } from "firebase/auth";
 import { getAnalytics, isSupported, type Analytics } from "firebase/analytics";
 
+const fallbackFirebaseConfig = {
+  apiKey: "AIzaSyDjsm8OqQ0z23oVP3J8rL2TX_jdrP19X20",
+  authDomain: "writers-forge-14384.firebaseapp.com",
+  projectId: "writers-forge-14384",
+  storageBucket: "writers-forge-14384.firebasestorage.app",
+  messagingSenderId: "951490206501",
+  appId: "1:951490206501:web:287680497f647cbac218cc",
+  measurementId: "G-QPJR605CGK",
+};
+
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || fallbackFirebaseConfig.apiKey,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || fallbackFirebaseConfig.authDomain,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || fallbackFirebaseConfig.projectId,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || fallbackFirebaseConfig.storageBucket,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || fallbackFirebaseConfig.messagingSenderId,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || fallbackFirebaseConfig.appId,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || fallbackFirebaseConfig.measurementId,
 };
 
 const hasFirebaseConfig = Boolean(
